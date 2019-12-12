ca1954555fa6584f8868437f08c3608314971098
verse <verseesgianni@outlook.com>
2019-12-12 14:55:39 +0100

    SPELERS: - attributen - constructor - getters voor score en gespeeldeBeurten - setters voor naam, score en gespeeldeBeurten

--- src/MemoryConsolePckg/Speler.java
+++ src/MemoryConsolePckg/Speler.java
@@ -3,5 +3,32 @@ package MemoryConsolePckg;
 public class Speler {
     private String naam;
     private int score;
+    private int gespeeldeBeurten;
 
+    //Constructor maken voor nieuwe speler aan te maken.
+    public Speler(String naam) {
+        this.naam = naam;
+    }
+
+    //setters voor score en gespeeldeBeurten
+    public void setScore(int score) {
+        this.score = score;
+    }
+
+    public void setGespeeldeBeurten(int gespeeldeBeurten) {
+        this.gespeeldeBeurten = gespeeldeBeurten;
+    }
+
+    //getters voor score, gespeeldeBeurten en naam
+    public String getNaam() {
+        return naam;
+    }
+
+    public int getScore() {
+        return score;
+    }
+
+    public int getGespeeldeBeurten() {
+        return gespeeldeBeurten;
+    }
 }
