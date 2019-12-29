b30e294eb0e94b411b3cfb90b0ad7686aa2ce1e3
verse <verseesgianni@outlook.com>
2019-12-29 21:52:22 +0100

    --29/12/19--21:51--Catch toegevoegd--Printen van spelbord aangepast-- ---VERDER OP VORIGE, OP HETZELFDE AANGEPAST---

--- src/MemoryConsolePckg/MemoryDemo.java
+++ src/MemoryConsolePckg/MemoryDemo.java
@@ -1,19 +1,29 @@
 package MemoryConsolePckg;
 
+import java.util.Scanner;
+
 public class MemoryDemo {
     public static void main(String[] args) {
-
         try{
             //Aanmaken van een nieuw spel
             Spel spel = new Spel();
             //Spel starten
             spel.StartSpel();
         }
-        catch (IllegalArgumentException i){
-            System.out.println("Gelieve een getal in te voeren.");
-        }
         catch (IllegalStateException s){
             System.out.println("Je kan enkel kiezen tussen niveau 1 en niveau 2.");
+            Spel spel2 = new Spel();
+            spel2.StartSpel();
+        }
+        catch (IllegalArgumentException i){
+            System.out.println("Gelieve een getal in te voeren");
+            Spel spel3 = new Spel();
+            spel3.StartSpel();
+        }
+        catch (Exception e){
+            System.out.println("Je gebruikte dit programma foutief.");
+            Spel spel4 = new Spel();
+            spel4.StartSpel();
         }
 
     }
