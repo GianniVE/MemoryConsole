c8f3bef1aeaf2ab1fc771baf772989a39969a04e
verse <verseesgianni@outlook.com>
2020-01-01 11:48:10 +0100

    --01/01/2020--11:47--Aanpassing van SpelbordNiveau1--nu werkt alles--

--- src/MemoryConsolePckg/MemoryDemo.java
+++ src/MemoryConsolePckg/MemoryDemo.java
@@ -17,19 +17,22 @@ public class MemoryDemo {
             System.out.println("Je kan enkel kiezen tussen niveau 1 en niveau 2.");
             System.out.println("Het spel zal worden herstart. Denk er aan om de de juiste waarden in te vullen.");
             Spel spel2 = new Spel();
-            spel2.StartSpel();
+            spel2.StartSpel(); //Als er tijdens het lopen van het spel dat hier is gestart nog een fout voorkomt
+            //zal hij wel een foutmelding geven.
         }
         catch (IllegalArgumentException i){
             System.out.println("Gelieve een getal in te voeren");
             System.out.println("Het spel zal worden herstart. Denk er aan om de de juiste waarden in te vullen.");
             Spel spel2 = new Spel();
-            spel2.StartSpel();
+            spel2.StartSpel();//Als er tijdens het lopen van het spel dat hier is gestart nog een fout voorkomt
+            //zal hij wel een foutmelding geven.
         }
         catch (Exception e){
             System.out.println("Je gebruikte dit programma foutief.");
             System.out.println("Het spel zal worden herstart. Denk er aan om de de juiste waarden in te vullen.");
             Spel spel2 = new Spel();
-            spel2.StartSpel();
+            spel2.StartSpel();//Als er tijdens het lopen van het spel dat hier is gestart nog een fout voorkomt
+            //zal hij wel een foutmelding geven.
         }
         //Niet in de finally, anders geeft hij dit ook als het spel correct is uitgespeeld.
 //        finally {
